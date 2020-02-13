6c575ed6e11f8eb7da2ad998690b60147d68131f
emard <vordah@gmail.com>
2020-02-13 09:20:01 +0100

    using hardware spi

--- test_st7789.py
+++ test_st7789.py
@@ -14,7 +14,7 @@ def main():
     gpio_sck = const(14)
     gpio_mosi = const(15)
     gpio_miso = const(2)
-    spi = machine.SPI(-1, baudrate=40000000, polarity=1, sck=machine.Pin(gpio_sck), mosi=machine.Pin(gpio_mosi), miso=machine.Pin(gpio_miso))
+    spi = machine.SPI(1, baudrate=40000000, polarity=1, sck=machine.Pin(gpio_sck), mosi=machine.Pin(gpio_mosi), miso=machine.Pin(gpio_miso))
     display = st7789.ST7789(
         spi, 240, 240,
         reset=machine.Pin(gpio_resn, machine.Pin.OUT),
