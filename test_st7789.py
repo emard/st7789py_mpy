ea7556a6caa63791b9c069271af6be022fdd1c98
emard <vordah@gmail.com>
2020-02-13 01:39:22 +0100

    pinout for ulx3s and software spi

--- test_st7789.py
+++ test_st7789.py
@@ -5,14 +5,20 @@ import random
 import machine
 import st7789py as st7789
 import time
-
+from micropython import const
 
 def main():
-    spi = machine.SPI(1, baudrate=40000000, polarity=1)
+    gpio_csn = const(17)
+    gpio_resn = const(26)
+    gpio_dc = const(16)
+    gpio_sck = const(14)
+    gpio_mosi = const(15)
+    gpio_miso = const(2)
+    spi = machine.SPI(-1, baudrate=40000000, polarity=1, sck=machine.Pin(gpio_sck), mosi=machine.Pin(gpio_mosi), miso=machine.Pin(gpio_miso))
     display = st7789.ST7789(
         spi, 240, 240,
-        reset=machine.Pin(5, machine.Pin.OUT),
-        dc=machine.Pin(2, machine.Pin.OUT),
+        reset=machine.Pin(gpio_resn, machine.Pin.OUT),
+        dc=machine.Pin(gpio_dc, machine.Pin.OUT),
     )
     display.init()
 
@@ -26,3 +32,4 @@ def main():
         )
         # Pause 2 seconds.
         time.sleep(2)
+
